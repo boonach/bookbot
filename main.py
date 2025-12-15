f015fc266e95757fd7c0df17c35ceed0e3f83b41
boonach <taylorbonachea@gmail.com>
2025-12-15 16:53:38 -0500

    fixing character sorting and making report

--- main.py
+++ main.py
@@ -5,13 +5,29 @@ def get_book_text(file_path):
         return book_contents
 
 #analyze book
-from stats import count_words, count_chars, sort_dict
+from stats import count_words, count_chars, sort_on, sort_char
+
 
 def main():
     file_path = "books/frankenstein.txt"
     book_contents = get_book_text(file_path)
     num_words = count_words(book_contents)
     char_count = count_chars(book_contents)
+    sort_char_count = sort_char(char_count)
+    return file_path, num_words, char_count, sort_char_count
+
+def print_report(file_path, num_words, sort_char_count):
+    print("========== BOOKBOT ==========")
+    print(f"Analyzing book found at {file_path}")
+    
+    print("--------- Word Count --------")
     print(f"Found {num_words} total words")
-    char_count.sort(reverse = True, key = sort_dict)
-main()
+
+    print("------ Character Count ------")
+    for char, count in sort_char_count.items():
+        print(f"{char}: {count}")
+    
+    print("============ END ============")
+
+file_path, num_words, char_count, sort_char_count = main()    
+print_report(file_path, num_words, sort_char_count)
--- stats.py
+++ stats.py
@@ -13,5 +13,17 @@ def count_chars(book_contents):
             char_counts[lowered] = 1
     return char_counts
 
-def sort_dict(items):
-    return items["num"]
+def sort_on(item):
+    return item[1]
+def sort_char(char_count):
+    filtered = {
+        char: count
+        for char, count in char_count.items()
+        if char.isalpha()
+    }
+    sorted_items = sorted(
+        filtered.items(),
+        key= sort_on,
+        reverse = True
+    )
+    return dict(sorted_items)
