b9c213e7152280abe51df1c73b786770a5705622
boonach <taylorbonachea@gmail.com>
2025-12-19 11:29:44 -0500

    Finalizing bookbot, adding sys args for filepath

--- main.py
+++ main.py
@@ -6,28 +6,23 @@ def get_book_text(file_path):
 
 #analyze book
 from stats import count_words, count_chars, sort_on, sort_char
+import sys
 
+print("Usage: python3 main.py <path_to_book>")
 
 def main():
-    file_path = "books/frankenstein.txt"
+    file_path = sys.argv[1]
     book_contents = get_book_text(file_path)
     num_words = count_words(book_contents)
     char_count = count_chars(book_contents)
     sort_char_count = sort_char(char_count)
-    return file_path, num_words, char_count, sort_char_count
-
-def print_report(file_path, num_words, sort_char_count):
     print("========== BOOKBOT ==========")
     print(f"Analyzing book found at {file_path}")
-    
     print("--------- Word Count --------")
     print(f"Found {num_words} total words")
-
     print("------ Character Count ------")
     for char, count in sort_char_count.items():
         print(f"{char}: {count}")
-    
     print("============ END ============")
 
-file_path, num_words, char_count, sort_char_count = main()    
-print_report(file_path, num_words, sort_char_count)
+main()
